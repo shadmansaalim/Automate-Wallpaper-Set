6652401129721a69e7cce2c87fdf4316495f2c29
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-10-14 19:36:02 +1100

    Response content decoded

--- auto_wallpaper.py
+++ auto_wallpaper.py
@@ -8,7 +8,12 @@ import requests
 api_url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
 
 # Fetching the data from api with a GET request and storing response
-response = requests.get(api_url)
 
-# Printing the response
-print(response)
+response = requests.get(api_url)
+content = response.content
+
+# Decoding the content from bytes to string
+decoded_content = content.decode('UTF-8')
+
+# Printing the content
+print(decoded_content)
