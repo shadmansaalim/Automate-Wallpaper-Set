4cd89fecae526793dd4aef141166c4b7cc45f80b
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-10-14 19:41:01 +1100

    Decoded string content converted to dictinoary using json loads method and image downloaded by URL

--- auto_wallpaper.py
+++ auto_wallpaper.py
@@ -1,8 +1,9 @@
 """
 Download and change laptop wallpaper automatically
 """
-# Requests Libraray
+# Libraries
 import requests
+import json
 
 # Open API
 api_url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
@@ -15,5 +16,14 @@ content = response.content
 # Decoding the content from bytes to string
 decoded_content = content.decode('UTF-8')
 
-# Printing the content
-print(decoded_content)
+# Convert string to JSON
+dict_content = json.loads(decoded_content)
+
+# Get image url from content
+image_url = dict_content['url']
+
+print(image_url)
+
+# Download the image
+res = requests.get(image_url)
+print(res)
