109b4e549bd930e7addf85a630bfb33906eca13a
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-10-14 19:32:59 +1100

    Requests library init, api url fetched and data consoled to check the response

--- auto_wallpaper.py
+++ auto_wallpaper.py
@@ -1,3 +1,14 @@
 """
 Download and change laptop wallpaper automatically
 """
+# Requests Libraray
+import requests
+
+# Open API
+api_url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+
+# Fetching the data from api with a GET request and storing response
+response = requests.get(api_url)
+
+# Printing the response
+print(response)
